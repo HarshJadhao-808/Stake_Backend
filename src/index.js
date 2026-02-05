d38e4fb1c9678fb62bdb959c4eb97d3b24e63727
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2026-02-05 19:43:24 +0530

    Stake Backend Completed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 import userRouter from "./Router/Userrouter.js";
 import dbconnect from "./Config/db.js";
 import adminRouter from "./Router/Adminrouter.js";
-
+import cors from 'cors'
 dotenv.config()
 const app = express()
+app.use(cors())
 app.use(express.json())
-
 dbconnect(process.env.mongo_url);
 
 app.use("/user",userRouter)
